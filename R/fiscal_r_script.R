44ac850f99bf837a2b59ea72337718d637e58ab2
dephilli <dephilli@gmail.com>
2022-01-27 12:41:21 -0900

    First Commit

---
+++ R/fiscal_r_script.R
@@ -0,0 +1,15 @@
+fiscal<-function(d){
+  y<-as.numeric(format(d, "%Y"))
+  m<-as.numeric(format(d, "%m"))
+  ifelse(m<=6,y,y+1)
+}
+
+fy_start<-function(y){
+  fy_start<-as.Date(paste0(yr-1,"-07-01"))
+  return(fy_start)
+}
+
+fy_end<-function(y){
+  fy_end<-as.Date(paste0(yr,"-06-30"))
+  return(fy_end)
+}
